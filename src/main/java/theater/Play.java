20c826391accef485f8d09720997ae6148d6591d
Evelyn <evelyncjin@gmail.com>
2025-11-23 23:43:15 -0500

    finished resolving checkstyle for Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,52 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type.
+ *
+ * @null Both name and type may be null.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    /**
+     * The name of the play.
+     *
+     * @null This value may be null.
+     */
+    private String name;
 
+    /**
+     * The type of the play, such as "tragedy" or "comedy".
+     *
+     * @null This value may be null.
+     */
+    private String type;
+
+    /**
+     * Creates a new play with the given name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the name of the play.
+     *
+     * @return the play name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     *
+     * @return the play type
+     */
+    public String getType() {
+        return type;
     }
 }
